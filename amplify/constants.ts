1de5d9d0f5119dfa2f8bfb69bfdbd3c6958edce1
Apollo Rowe <1apollo.rowe@gmail.com>
2024-11-03 23:55:35 -0500

    bug fix

--- amplify/constants.ts
+++ amplify/constants.ts
@@ -1,6 +1,8 @@
+import { secret } from "@aws-amplify/backend";
+
 export const stackConstants = {
-    emailQueueName: 'amplify-jamesfrenchphoto-apollo-sandbox-da786b10-EmailQueue9C1DA90F-4qnPxpXc2Vlf',
-    stateMachineArn: 'arn:aws:states:us-east-1:911167908910:stateMachine:EmailStepFunctionEmailStateMachineF23B2927-oJUPNzx8p9FF',
-    tempTokensArn: 'arn:aws:dynamodb:us-east-1:911167908910:table/TemporaryCreateUsersTokens-syymw3momfhyfcpmjctmbhhsie-NONE',
-    tempTokensDbName: 'TemporaryCreateUsersTokens-syymw3momfhyfcpmjctmbhhsie-NONE',
+    emailQueueName: secret('emailQueue') ?? 'amplify-jamesfrenchphoto-apollo-sandbox-da786b10-EmailQueue9C1DA90F-4qnPxpXc2Vlf',
+    stateMachineArn: secret('stateMachineArn') ?? 'arn:aws:states:us-east-1:911167908910:stateMachine:EmailStepFunctionEmailStateMachineF23B2927-oJUPNzx8p9FF',
+    tempTokensArn: secret('tempTokensArn') ?? 'arn:aws:dynamodb:us-east-1:911167908910:table/TemporaryCreateUsersTokens-syymw3momfhyfcpmjctmbhhsie-NONE',
+    tempTokensDbName: secret('tempTokensDbName') ?? 'TemporaryCreateUsersTokens-syymw3momfhyfcpmjctmbhhsie-NONE',
 }
