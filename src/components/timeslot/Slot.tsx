e1ccd87941c9da9105038dea0b767a06fe1985b4
Apollinaris Rowe <1apollo.rowe@gmail.com>
2024-12-17 01:48:08 -0500

    Release 2.0.1 Patch 2

--- src/components/timeslot/Slot.tsx
+++ src/components/timeslot/Slot.tsx
@@ -3,6 +3,9 @@ import { Participant, Timeslot, UserTag } from "../../types";
 import { generateClient } from "aws-amplify/api";
 import { Schema } from "../../../amplify/data/resource";
 import { formatTime } from "../../utils";
+import { HiOutlineCalendar } from "react-icons/hi2";
+import { EventAttributes, createEvent } from 'ics'
+import { DateTime } from 'luxon'
 
 const client = generateClient<Schema>()
 
@@ -13,6 +16,52 @@ export interface SlotProps {
 
 export function createTimeString(timeslot: Timeslot){
     return timeslot.start.toLocaleTimeString("en-us", { timeZone: 'America/Chicago' }) + " - " + timeslot.end.toLocaleTimeString("en-us", { timeZone: 'America/Chicago' })
+}
+
+export function createTimeslotEvent(timeslot: Timeslot, participant: Participant){
+    if(!timeslot.register){
+        return (<></>)
+    }
+    const delta = new Date(timeslot.end.getTime() - timeslot.start.getTime())
+    const startDateTime = DateTime.fromObject(
+        { year: timeslot.start.getFullYear(), month: timeslot.start.getMonth() + 1, day: timeslot.start.getDate(), hour: timeslot.start.getHours(), minute: timeslot.start.getMinutes()},
+        { zone: 'America/Chicago' }
+    )
+    const startUTC = startDateTime
+    
+    const calendarEvent: EventAttributes = {
+        start: [startUTC.year, startUTC.month, startUTC.day, startUTC.hour, startUTC.minute],
+        duration: { hours: delta.getHours(), minutes: delta.getMinutes() },
+        title: `${participant.firstName} ${participant.lastName}`,
+        description: `Photoshoot for your participant`,
+        url: 'https://www.jamesfrenchphoto.com',
+        geo: { lat: 32.813040, lon: -96.803810 },
+        location: '3624 Oak Lawn Ave # 222, Dallas, TX 75219',
+        status: 'CONFIRMED',
+        categories: ['James French Photography', 'Photoshoot', 'La Fiesta 2025', 'Debutante', 'Headshot'],
+        busyStatus: 'BUSY',
+        organizer: { name: 'James French Photography', email: 'camille@jfrenchphotography.com' },
+        attendees: [
+            { email: timeslot.register, rsvp: true, role: 'REQ-PARTICIPANT' }
+        ],
+        alarms: [{action: 'display', trigger: { minutes: 30, before: true }}]
+    }
+
+    let objectURL: string | undefined
+    createEvent(calendarEvent, (error, value) => {
+        if(error) {
+            console.log(error)
+            return
+        }
+        const calendarInvite = new Blob([value], { type: 'text/calendar;charset=utf-8' })
+        objectURL = URL.createObjectURL(calendarInvite)
+    })
+
+    return (
+        <a href={objectURL} download={`LAF_2025_Headshot_${participant.firstName}_${participant.lastName}.ics`}>
+            <HiOutlineCalendar size={24} />
+        </a>
+    )
 }
 
 export const SlotComponent: FC<SlotProps> = ({ timeslot, displayRegister }) => {
@@ -63,6 +112,7 @@ export const SlotComponent: FC<SlotProps> = ({ timeslot, displayRegister }) => {
                             preferredName: userProfileResponse.data.participantPreferredName ?? undefined,
                             contact: false,
                             userTags: []
+                            
                         }
                     }
                 }
@@ -79,7 +129,7 @@ export const SlotComponent: FC<SlotProps> = ({ timeslot, displayRegister }) => {
     })
 
     return (
-        <div className={`flex flex-col border border-black justify-center items-center rounded-lg py-2 px-4 text-${tag?.color ?? 'black'}`}>
+        <div className={`flex flex-col relative border border-black justify-center items-center rounded-lg py-2 px-4 text-${tag?.color ?? 'black'}`}>
             <span>{"Time: " + createTimeString(timeslot)}</span>
             {
                 displayRegister ? 
@@ -92,6 +142,15 @@ export const SlotComponent: FC<SlotProps> = ({ timeslot, displayRegister }) => {
                             </span>
                         )
                     ) : (<></>)
+            }
+            {
+                displayRegister ? (
+                    timeslotParticipant !== undefined ? (
+                        <div className="absolute inset-y-0 right-0 self-center me-2">
+                            {createTimeslotEvent(timeslot, timeslotParticipant)}
+                        </div>
+                    ) : (<></>)
+                ) : (<></>)
             }
         </div>)
 }
