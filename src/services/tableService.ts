c0a1e00ba833fdbda3a9208458c44fcc66a59f5f
Apollinaris Rowe <1apollo.rowe@gmail.com>
2025-03-08 16:27:22 -0500

    Update tableService.ts
    get table query

--- src/services/tableService.ts
+++ src/services/tableService.ts
@@ -28,12 +28,52 @@ async function getAllTableGroups(client: V6Client<Schema>, options: GetAllTableG
         }
 
         return mappedGroup
-    }
+    }))
 
     return mappedGroups
 }
 
+interface GetTableOptions {
+    logging: boolean,
+    metrics: boolean
+}
+async function getTable(client: V6Client<Schema>, id?: string, options?: GetTableOptions) {
+    if(!id) return
+    const tableResponse = await client.models.Table.get({ id: id })
+    if(!tableResponse || !tableResponse.data) return
+
+    const mappedColumns: TableColumn[] = await Promise.all((await tableResponse.data.columns()).map(async (column) => {
+        const color: ColumnColorDisplay[] = (await column.color()).map((color) => {
+            const mappedColor: ColumnColorDisplay = {
+                ...color,
+                bgColor: color.bgColor ?? undefined,
+                textColor: color.textColor ?? undefined,
+            }
+            return mappedColor
+        })
+        const mappedColumn: TableColumn = {
+            ...column,
+            values: column.values ? column.values as string[] : [],
+            choices: column.choices ? column.choices as string[] : [],
+            type: column.type ?? 'value' as 'value',
+            display: column.display ?? true,
+            tag: column.tag ? column.tag as string[] : [],
+            columnColorDisplay: color,
+        }
+    }))
+    const mappedTable: Table = {
+        ...tableResponse.data,
+        columns: mappedColumns,
+     }
+    return mappedTable
+}
+
 export const getAllTableGroupsQueryOptions = (options?: GetAllTableGroupsOptions) => queryOptions({
     queryKey: ['TableGroups', client, options],
-    queeyFn: getAllTableGroups(client, options)
+    queryFn: getAllTableGroups(client, options)
+})
+
+export const getTableQueryOptions = (id?: string, options?: GetTableOptions) => queryOptipns({
+    queryKey: ['Table', client, id, options],
+    queryFn: getTable(client, id, options)
 })
